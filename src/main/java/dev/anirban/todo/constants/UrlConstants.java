ab0ff57d0cb24d37c6074c18765eaf5846bb2693
Anirban Basak <jeetbasak2002@gmail.com>
2024-12-02 04:45:36 +0530

    feat - Configured the Spring Security Files and passwords for rapid development.

---
+++ src/main/java/dev/anirban/todo/constants/UrlConstants.java
@@ -0,0 +1,11 @@
+package dev.anirban.todo.constants;
+
+
+public class UrlConstants {
+
+    // User Endpoints
+    public static final String CREATE_USER = "/users";
+    public static final String FIND_ALL_USER = "/users";
+    public static final String FIND_USER_BY_ID = "/users/{id}";
+    public static final String DELETE_USER_BY_ID = "/users/{id}";
+}
